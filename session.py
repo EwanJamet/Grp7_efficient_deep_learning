fa7433401528d57a84b7e01381c1fc492817d7bb
Cactus <151628368+EwanJamet@users.noreply.github.com>
2024-02-14 22:04:29 +0100

    Create session.py

---
+++ session.py
@@ -0,0 +1,297 @@
+from torchvision.datasets import CIFAR10
+import numpy as np 
+import torchvision.transforms as transforms
+import torch 
+from torch.utils.data.dataloader import DataLoader
+from vgg import *
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import argparse
+
+#hyperparameter
+
+TEST = False
+# TEST = True
+
+len_epoch = 100
+Learning_rate = 1
+Batch_size = 32
+num_samples_subset = 500
+
+NAME_FILE = "VG11_50000_100epoch_scheduler"
+
+SCHEDULER_ON = True
+
+## We set a seed manually so as to reproduce the results easily
+seed  = 2147483647
+
+global list_train 
+global list_valid 
+list_train = np.array([])
+list_valid = np.array([])
+
+#parameters
+
+device = torch.device("cuda:0")
+use_cuda = torch.cuda.is_available()
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=Learning_rate, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--model', default="VGG11", type=str,
+                    help='model type (default: ResNet18)')
+parser.add_argument('--name', default='0', type=str, help='name of run')
+parser.add_argument('--seed', default=0, type=int, help='random seed')
+parser.add_argument('--batch-size', default=Batch_size, type=int, help='batch size')
+parser.add_argument('--epoch', default=len_epoch, type=int,
+                    help='total epochs to run')
+parser.add_argument('--no-augment', dest='augment', action='store_false',
+                    help='use standard augmentation (default: True)')
+parser.add_argument('--decay', default=1e-4, type=float, help='weight decay')
+parser.add_argument('--alpha', default=1., type=float,
+                    help='mixup interpolation coefficient (default: 1)')
+args = parser.parse_args()
+
+
+## Normalization adapted for CIFAR10
+normalize_scratch = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+
+# Transforms is a list of transformations applied on the 'raw' dataset before the data is fed to the modelwork. 
+# Here, Data augmentation (RandomCrop and Horizontal Flip) are applied to each batch, differently at each epoch, on the training set data only
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    normalize_scratch,
+])
+
+### The data from CIFAR10 will be downloaded in the following folder
+rootdir = './data/cifar10'
+
+c10train = CIFAR10(rootdir,train=True,download=True,transform=transform_train)
+c10test = CIFAR10(rootdir,train=False,download=True,transform=transform_test)
+
+trainloader = DataLoader(c10train,batch_size=Batch_size,shuffle=True) 
+testloader = DataLoader(c10test,batch_size=Batch_size) 
+
+## number of target samples for the final dataset
+num_train_examples = len(c10train)
+
+
+## Generate a list of shuffled indices ; with the fixed seed, the permutation will always be the same, for reproducibility
+indices = list(range(num_train_examples))
+np.random.RandomState(seed=seed).shuffle(indices)## modifies the list in place
+
+## We define the Subset using the generated indices 
+c10train_subset = torch.utils.data.Subset(c10train,indices[:num_samples_subset])
+print(f"Initial CIFAR10 dataset has {len(c10train)} samples")
+print(f"Subset of CIFAR10 dataset has {len(c10train_subset)} samples")
+
+# Finally we can define anoter dataloader for the training data
+trainloader_subset = DataLoader(c10train_subset,batch_size=32,shuffle=True)
+### You can now use either trainloader (full CIFAR10) or trainloader_subset (subset of CIFAR10) to train your modelworks.
+
+data = trainloader_subset 
+criterion = nn.CrossEntropyLoss()
+model = VGG('VGG11')
+model = model.to(device)
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9,
+                      weight_decay=args.decay)
+
+#scheduler
+
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+# scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=args.lr,step_size_up=20,mode="triangular2")
+lrs = []
+
+
+
+def m_test (model):
+    model.eval()
+    correct = 0
+    total = 0
+
+    for batch_index, (inputs,labels) in enumerate(testloader):
+        
+        inputs,labels  = inputs.to(device),labels.to(device)
+
+        outputs = model(inputs)
+        outputs = outputs.to(device)
+
+        loss = criterion(outputs, labels)
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += predicted.eq(labels.data).cpu().sum()
+        acc += 100.*correct/total
+
+    return acc/len(testloader)
+
+
+
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).to(device)
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = (lam * x + (1 - lam) * x[index, :])
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
+
+def infer_mixup(inputs,labels,correct,total,TEST):
+    
+    inputs, labels = inputs.cuda(), labels.cuda()
+
+    inputs, targets_a, targets_b, lam = mixup_data(inputs, labels,args.alpha, use_cuda)
+    inputs, targets_a, targets_b = map(Variable, (inputs,targets_a, targets_b))
+    outputs = model(inputs)
+
+    loss = mixup_criterion(criterion, outputs, targets_a, targets_b, lam)
+
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (lam * predicted.eq(targets_a.data).cpu().sum().float()
+            + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
+    if TEST :
+        acc = 100.*correct/total
+        return loss,acc
+    else :
+        return loss,correct,total
+
+
+
+def infer_classic(inputs,labels,correct,total,TEST):
+    
+    inputs,labels  = inputs.to(device),labels.to(device)
+    outputs = model(inputs)
+    outputs = outputs.to(device)
+    loss = criterion(outputs, labels)
+
+    if TEST:
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += predicted.eq(labels.data).cpu().sum()
+        acc = 100.*correct/total
+        return loss,acc
+    else:
+        return loss
+
+
+
+def m_train():
+    global list_train
+    global list_valid
+    
+    reset_lr = SCHEDULER_ON
+
+    for epoch in range(len_epoch):
+
+        # Training set initialization
+        running_loss = 0.0
+        running_loss_valid = 0.0
+        correct = 0
+        total = 0
+
+        model.train()
+        TEST = False
+
+        #training set infer
+        for batch_index, (inputs,labels) in enumerate(trainloader_subset):
+            
+            if epoch < len_epoch/2 :
+                loss = infer_classic(inputs,labels,correct,total,TEST)
+            else :
+                if reset_lr:
+                    optimizer.param_groups[0]["lr"] = 0.1
+                    reset_lr = False
+                loss,correct,total = infer_mixup(inputs,labels,correct,total,TEST)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()    
+
+            # statistics
+            running_loss += loss.item()
+
+
+        #scheduler
+        if SCHEDULER_ON:
+            lrs.append(optimizer.param_groups[0]["lr"])
+            scheduler.step(running_loss)
+            #print lr
+            print("lrs = ",lrs[-1])
+
+        
+        #validation test
+        running_acc_valid  = 0
+        running_loss_valid  = 0
+        correct = 0
+        total = 0
+
+        model.eval()
+        TEST = True
+
+        #validation infer
+        for batch_index, (inputs,labels) in enumerate(testloader):
+            if epoch < len_epoch/2 :
+                loss_valid,acc_valid = infer_classic(inputs,labels,correct,total,TEST)
+            else :
+                loss_valid,acc_valid = infer_mixup(inputs,labels,correct,total,TEST)
+            
+            #accumulation 
+            running_acc_valid  += acc_valid
+            running_loss_valid += loss_valid.item()     
+
+        #print stat loss
+        print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_index + 1, running_loss / len(trainloader_subset)))
+        list_train = np.append(list_train,running_loss/len(trainloader_subset))
+
+        #print stat acc
+        print('[%d, %5d] acc_valid: %.3f' % (epoch + 1, batch_index + 1, running_acc_valid / len(testloader))) 
+        list_valid = np.append(list_valid,running_loss_valid/len(trainloader_subset))
+        list_valid = np.append(list_valid,running_acc_valid/len(testloader))
+        
+        
+        
+
+
+
+
+def m_save():
+    global list_train
+    global list_valid
+    torch.save(model, '/homes/e21jamet/Documents/efficient-deep-learning/models_cifar100/'+ NAME_FILE +'.pth')
+    np.save( '/homes/e21jamet/Documents/efficient-deep-learning/models_cifar100/'+ NAME_FILE +'_train_values.npy', list_train)
+    np.save( '/homes/e21jamet/Documents/efficient-deep-learning/models_cifar100/'+ NAME_FILE +'_valid_values.npy', list_valid)
+
+
+if not TEST:
+    m_train()
+    m_save()
+
+if TEST :
+    model = torch.load('/homes/e21jamet/Documents/efficient-deep-learning/models_cifar100/'+ NAME_FILE +'.pth')
+    acc = m_test(model)
+    print('acc =',acc.numpy())
+
