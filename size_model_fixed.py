ec67c035d4f84de61c48aa55df7e8ae3385aa6f7
Remi-Vassal <152400203+Remi-Vassal@users.noreply.github.com>
2024-03-05 22:13:08 +0100

    Update size_model_fixed.py

--- size_model_fixed.py
+++ size_model_fixed.py
@@ -16,27 +16,21 @@ import time
 import matplotlib.pyplot as plt
 import binaryconnect 
 
+
 model_file = "/homes/r21vassa/Documents/eff_dl/vgg16/save_model/save_model_state_final.pth"
 model = torch.load(model_file, map_location=torch.device('cpu'))
 
-TEST = True
-
-if TEST:
-    '''
-    params = ()
-    for name, module in model.named_modules():
-            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-                params += ((module, "weight"),)
-    prune.global_unstructured(params, pruning_method=prune.L1Unstructured, amount=0.2)
-    '''
-    s = 0
-    for name, module in model.named_modules():
-            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-                s += torch.sum(module.weight == 0)
-    print("s = ", s)
-
+s = 0
+for name, module in model.named_modules():
+        if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+            s += torch.sum(module.weight == 0)
 
 def count_parameters(model):
     return sum(p[torch.abs(p)>10**-15].numel() for p in model.parameters() if( p.requires_grad))
 
-print(count_parameters(model))
+c = count_parameters(model)
+
+print(c)
+
+print(
+    "Sparsity in fc3.weight: {:.2f}%".format((100. * s)/ c))
