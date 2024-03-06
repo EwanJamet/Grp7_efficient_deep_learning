ba1e4e13db514d74f87ad8a1c0cf97b1f202408d
Remi-Vassal <152400203+Remi-Vassal@users.noreply.github.com>
2024-03-06 15:09:51 +0100

    Add files via upload

---
+++ session5/calcul_param.py
@@ -0,0 +1,37 @@
+from torchvision.datasets import CIFAR10
+import numpy as np 
+import torchvision.transforms as transforms
+import torch 
+from torch.utils.data.dataloader import DataLoader
+from vgg import *
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
+import argparse
+import os
+import pickle
+import time
+import matplotlib.pyplot as plt
+import binaryconnect 
+
+model_file = "/homes/r21vassa/Documents/eff_dl/test_vgg/save_model/save_model_state_final.pth"
+model = torch.load(model_file, map_location=torch.device('cpu'))
+
+file_stats = os.stat(model_file)
+
+s = 0
+for name, module in model.named_modules():
+        if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+            s += torch.sum(module.weight == 0)
+
+
+def count_parameters(model):
+    return sum(p[torch.abs(p)>=0].numel() for p in model.parameters() if( p.requires_grad))
+
+c = count_parameters(model)
+
+print("nb total weight", c)
+
+print("Sparsity in model: {:.2f}%".format((100. * s)/ c))
